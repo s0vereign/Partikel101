a4de610b4218fbe9fae524b6f200693032a824d4
s0vereign <maxboehme@freenet.de>
2014-09-16 10:48:59 +0200

    First version of the Drawer done!

--- Drawer.py
+++ Drawer.py
@@ -6,13 +6,15 @@ Created on Mon Sep 15 16:29:15 2014
 """
 
 
-from matplotlib import pyplot as pp 
+import matplotlib as mp
 from scipy import numpy as np
 class Drawer:
     
     
-    r = np.array();
-    
+    r = [];
+    x = [];    
+    y = [];
+    z = [];    
     
     def __init__(self):
     
@@ -32,4 +34,19 @@ class Drawer:
         
     def Draw():
         
-        pass;
+        fig = plt.figure();
+        for i in range(len(r)):
+            
+            vh = r[i];
+            
+            x.append(vh[1]);
+            y.append(vh[2]);
+            z.append(vh[3]);
+                      
+        ax = fig.gca(projection='3d');
+        ax.plot(x,y,z,label = 'Tajectory');
+        ax.legend();
+        plt.show()
+            
+            
+        
