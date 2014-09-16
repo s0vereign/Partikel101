2de0dcf3c6cd133805b15e6b01aea88a952c59cb
s0vereign <maxboehme@freenet.de>
2014-09-16 11:26:11 +0200

    Added some features

---
+++ exec.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 16 11:02:26 2014
+
+@author: max
+
+ This is the executing script for 
+ Project Particle 101
+
+
+"""
+
+from Computer import Computer
+from Drawer   import Drawer
+from Particle import Particle
+from Field    import Field
+
+
+
+
+r = Drawer()
