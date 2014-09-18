Fixed Units completly but precision lost

Computer.py
@@ -40,7 +40,8 @@ class Computer:
        
         
     def start(self, E, B, particle, start, end):
-      
+        r = particle.getBeta(particle.getCurrentcp())*particle.getGamma(particle.getBeta(particle.getCurrentcp()))*particle.getM()/(Constants.c*0.2)
+        print(r)        
         for i in range(start, end):
             for t in np.arange(i, i+1, self.dt):
                 self.step(E, B, particle, t)

exec.py
@@ -42,7 +42,7 @@ m = Constants.mp
 q = Constants.e
 cp0 = np.array([np.sqrt(20e6**2 - m**2),0,0])
 tStart = 0
-tEnd = 20
+tEnd = 50
 dt = 1e-3
 
 E = Field(E_Feld)