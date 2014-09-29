d66ed8df4915e33fea248a12f0fca6349d57df99
epsilonbelowzero <duda.land@gmail.com>
2014-09-29 12:16:15 +0200

    another try of penning-trap

--- exec.py
+++ exec.py
@@ -19,47 +19,41 @@ from Particle import Particle
 from Field    import Field
 from Constants import Constants as cons
 
+import sys
 
+U0 = 1e4#V
+z0 = 0.02
+roh0 = 0.01
+B = 5
+d = np.sqrt(1/2 * (z0**2 + roh0**2 / 2))
 
 r0 = np.array([0,0,0])
-m = cons.me
-q = cons.e
-#whats's the idea in calculating cp0 this way??!?!?!?
-cp0 = 0.8 * np.array([1,0,0]) 
+m = 52019.47214708469#MeV
+q = 6
+cp0 = np.sqrt((0.28204529073527773e-6 + m**2)**2 - m**2) * np.array([1,0,0]) 
 tStart = 0
 tEnd = 1e-9
 dt = 1e-11
 
 def E_Feld(x,y,z, t):
-    d1 = cons.d1
-    d2 = cons.d2
-    d1b = cons.d1b
-    d2b = cons.d2b
+    roh = np.sqrt(x**2 + y**2)
+    #E = - U0 / (2 * d**2) * (2 * z - roh)
+    Ez = - U0 / (2 * d**2) *  2 * z
+    Eroh = U0 / (2 * d**2) *  roh
+    try:
+        Ex = Eroh / (np.cos(np.arctan2(y,x)))
+        Ey = Eroh / (np.sin(np.arctan2(y,x)))
+    except:
+        print(sys.exc_info())
+        print(type(x))
+        print(type(y))
     
-    
-    r = np.array([x,y,z]);
-    rbetr = np.linalg.norm(r);
-    s1 = 1/(rbetr+0.5*d1b)**2-1/(rbetr-0.5*d1b)**2#Calculate the 
-    s2 = 1/(rbetr+0.5*d2b)**2-1/(rbetr-0.5*d2b)**2#reziproke terms
-    
-    sg = s1+s2                  #Calculating the reziproke terms
-    Eges = q**2/(4*math.pi*cons.e0)*sg
-    # so much for the norm of the field now let's get to the 
-    # direction
-    
-    er1 = r + 0.5*d1/(np.linalg.norm(r+0.5*cons.d1))+ r-0.5*d1/np.linalg.norm(r-0.5*d1)
-    er2 = r + 0.5*cons.d2/np.linalg.norm(r+0.5*cons.d1) + r - 0.5*d2/np.linalg.norm(r-0.5*d2)
-    er = er1 + er2;
-    #calculated the direction of the field
-    
-    
-    
-    return Eges*er;
+    return [Ex, Ey, Ez];
 
 def B_Feld(x,y,z, t):
     Bx = 0;
     By = 0;
-    Bz = 0.02;
+    Bz = B;
     
     return [Bx, By, Bz];
 
@@ -69,7 +63,7 @@ E = Field(E_Feld)
 B = Field(B_Feld)
 particle = Particle(r0, cp0, m, q)
 
-print("Radius [m]:", particle.getBeta()*particle.getGamma()*particle.getM()*1e6/(cons.c*np.linalg.norm(B.calcField(r0,0))))
+#print("Radius [m]:", particle.getBeta()*particle.getGamma()*particle.getM()*1e6/(cons.c*np.linalg.norm(B.calcField(r0,0))))
 
 
 comput = Computer(dt)
@@ -77,4 +71,4 @@ comput.start(E, B, particle, tStart, tEnd)
 
 r = Drawer()
 r.Draw(particle, tStart, tEnd, dt)
-print(particle.getKineticEnergy()[-1]);
+#print(particle.getKineticEnergy()[-1]);
