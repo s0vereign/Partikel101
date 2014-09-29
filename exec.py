443e469a9d2c8d6de0c2d9dfe1adc9df9800bd98
epsilonbelowzero <duda.land@gmail.com>
2014-09-29 13:29:54 +0200

    small fix

--- exec.py
+++ exec.py
@@ -19,41 +19,47 @@ from Particle import Particle
 from Field    import Field
 from Constants import Constants as cons
 
-import sys
-
 U0 = 1e4#V
 z0 = 0.02
 roh0 = 0.01
-B = 5
+B0 = 5
 d = np.sqrt(1/2 * (z0**2 + roh0**2 / 2))
 
-r0 = np.array([0,0,0])
+r0 = np.array([1.0,1.0,0.0])
 m = 52019.47214708469#MeV
 q = 6
 cp0 = np.sqrt((0.28204529073527773e-6 + m**2)**2 - m**2) * np.array([1,0,0]) 
 tStart = 0
-tEnd = 1e-9
+tEnd = 1e-7
 dt = 1e-11
 
 def E_Feld(x,y,z, t):
-    roh = np.sqrt(x**2 + y**2)
-    #E = - U0 / (2 * d**2) * (2 * z - roh)
-    Ez = - U0 / (2 * d**2) *  2 * z
-    Eroh = U0 / (2 * d**2) *  roh
+    
     try:
+        roh = np.sqrt(x**2 + y**2)
+        #E = - U0 / (2 * d**2) * (2 * z - roh)
+        Ez = - U0 / (2 * d**2) *  2 * z
+        Eroh = U0 / (2 * d**2) *  roh
         Ex = Eroh / (np.cos(np.arctan2(y,x)))
         Ey = Eroh / (np.sin(np.arctan2(y,x)))
-    except:
-        print(sys.exc_info())
-        print(type(x))
-        print(type(y))
+        
+        assert isinstance(x, np.float64)
+        assert isinstance(y, np.float64)
+        assert isinstance(Eroh, np.float64)
+        assert isinstance(roh, np.float64)
+    except BaseException, e:
+        print(e)
+        print(type(x),x)
+        print(type(y),y)
+        print(type(Eroh), Eroh)
+        print(type(roh), roh)
     
     return [Ex, Ey, Ez];
 
 def B_Feld(x,y,z, t):
     Bx = 0;
     By = 0;
-    Bz = B;
+    Bz = B0;
     
     return [Bx, By, Bz];
 
