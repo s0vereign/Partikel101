b9eb7e4d871211a219fb7b8bcc6fcddeadf25810
s0vereign <maxboehme@freenet.de>
2014-10-01 17:00:07 +0200

    Semistable traj.

--- exec.py
+++ exec.py
@@ -18,7 +18,7 @@ from Particle import Particle
 from Field    import Field
 from Constants import Constants as cons
 
-roh0 = 8e-4#m
+roh0 = 8e-1#m
 z0 = roh0 / np.sqrt(2)#m
 d = np.sqrt(1.0/2 * (z0**2 + roh0**2 / 2))
 #d = 10e-3#m
@@ -26,22 +26,26 @@ d = np.sqrt(1.0/2 * (z0**2 + roh0**2 / 2))
 m = cons.mp#MeV
 q = 1
 
-r = roh0#m
+r = roh0 * 2./3.#m
 
-n1 = 5e3
+n1 = 5e4
 n2 = 10
-r0 = np.array([1.0,1.0,1.0])
+r0 = np.array([0.0,0.0,.10])
+
+
 
 omega_plus = 1e10
-
-omega_c = omega_plus * (1 + 1.0 / n1)
+omega_c = omega_plus * (n1+1)
 omega_minus = omega_plus/n1
-omega_z = omega_plus/n2
+omega_z = omega_plus*n2
 
-B0 = (omega_c) * m * 1e6 / (q*cons.c**2)
-U0 = (omega_z)**2 * 1e6 *m / (q * cons.c**2) * d**2
-#U0 = 9e3
+
+B0 = (omega_c) * m   / (q*cons.c**2)
+#B0 = 1
+U0 = (omega_z)**2  *m / ((q)*cons.c**2) * d**2
+#U0 = 2e3
 cp0 = m*np.sqrt( 1.0 / (1 - omega_minus**2 * r**2 / cons.c**2) - 1) * np.array([1,0,0]) # m * sqrt(gamma**2 - 1) * e_cp0
+#cp0 = np.sqrt((20e8+m*cons.c**2)**2-(m*cons.c**2)**2)*np.array([1.,0.,0.])
 
 print("\omega_c = {:5.2e}".format(omega_c))
 print("\omega_+ = {:5.2e}".format(omega_plus))
@@ -55,7 +59,7 @@ print("U = {:5.2e}".format(U0))
 print("B = {:5.2e}".format(B0))
 
 tStart = 0
-tEnd = 1e-7
+tEnd = 1e-6
 dt = 1e-11
 
 
@@ -64,7 +68,7 @@ def E_Feld(x,y,z, t):
     Ey = U0 / (2 * d**2) * y
     Ez = - U0 / d**2 * z
 
-    #return [0,0,Ez]
+    #return [0,0,0]
     return [Ex, Ey, Ez];
 
 def B_Feld(x,y,z, t):
