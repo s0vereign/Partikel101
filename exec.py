db7c83ab8bf151e2259485eb76741fa2334e57a0
s0vereign <maxboehme@live.de>
2014-10-01 15:08:05 +0200

    Changed initial conditions

--- exec.py
+++ exec.py
@@ -18,7 +18,7 @@ from Particle import Particle
 from Field    import Field
 from Constants import Constants as cons
 
-roh0 = 8e-3#m
+roh0 = 8e-4#m
 z0 = roh0 / np.sqrt(2)#m
 d = np.sqrt(1.0/2 * (z0**2 + roh0**2 / 2))
 #d = 10e-3#m
@@ -26,23 +26,22 @@ d = np.sqrt(1.0/2 * (z0**2 + roh0**2 / 2))
 m = cons.mp#MeV
 q = 1
 
-r = roh0 * 2 / 3.0#m
+r = roh0#m
 
-n1 = 1e-4
-n2 = 1.0
-
+n1 = 5e3
+n2 = 10
 r0 = np.array([1.0,1.0,1.0])
 
-omega_plus = 1e1
+omega_plus = 1e10
 
 omega_c = omega_plus * (1 + 1.0 / n1)
-omega_minus = n1 * omega_plus
-omega_z = n2 * omega_plus
+omega_minus = omega_plus/n1
+omega_z = omega_plus/n2
 
-B0 = omega_c * m / (q*cons.c**2)
-U0 = (omega_plus / n2)**2 * m / (q * cons.c**2) * d**2
-
-cp0 = m*np.sqrt( 1.0 / (1 - omega_plus**2 * r**2 / cons.c**2) - 1) * np.array([1,0,0]) # m * sqrt(gamma**2 - 1) * e_cp0
+B0 = (omega_c) * m * 1e6 / (q*cons.c**2)
+U0 = (omega_z)**2 * 1e6 *m / (q * cons.c**2) * d**2
+#U0 = 9e3
+cp0 = m*np.sqrt( 1.0 / (1 - omega_minus**2 * r**2 / cons.c**2) - 1) * np.array([1,0,0]) # m * sqrt(gamma**2 - 1) * e_cp0
 
 print("\omega_c = {:5.2e}".format(omega_c))
 print("\omega_+ = {:5.2e}".format(omega_plus))
